66299159c7b5cd6386a3d4c450e9adc3a098cae9
0xankit <ankit.kr554@gmail.com>
2024-04-14 18:31:34 +0530

    configure bot tweet interval

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Twitter } from "./app.js";
 import { generateAndSaveImage } from "./helper/test.js";
 
 // Twitter BOT
-const twitter = new Twitter();
+// const twitter = new Twitter();
 // const tweetText = "Hello, this is a test. #test1";
 // twitter.sendTweet(tweetText);He
 // twitter.sendTweetWithMediasTagged(tweetText, ["1738769093560561664"]);
@@ -11,13 +11,19 @@ const twitter = new Twitter();
 
 // const image = await generateImage("Hello World");
 
-// generate image and text
-const mesages = await generateAndSaveImage();
-console.log("MESSAGES:", mesages)
-// for (let i = 0; i < mesages.length; i++) {
-//   const message = mesages[i];
-//   twitter.sendTweetWithMedias(message.tweetText, [message.image]);
-// }
+async function main() {
+    const twitter = new Twitter();
+    // generate image and text
+    const mesages = await generateAndSaveImage();
+    console.log("MESSAGES:", mesages)
+    for (let i = 0; i < mesages.length; i++) {
+      const message = mesages[i];
+      await twitter.sendTweetWithMedias(message.tweetText, [message.image]);
+    }
+}
 
+// run main function on interval of 30 minutes
+main();
+setInterval(main, 1000 * 60 * 1);
 
 // twitter.sendTweetWithMedias(tweetText, [image]);
