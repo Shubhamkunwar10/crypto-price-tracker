40d3e3bbdc36e429e355c0af631da0e3bd02e03f
0xankit <ankit.kr554@gmail.com>
2024-04-15 12:15:19 +0530

    add tweet interval

--- config.js
+++ config.js
@@ -7,11 +7,24 @@ export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 // CoinMarketCap Config
-export const COINMARKETCAP_ENDPOINT =
-  "https://pro-api.coinmarketcap.com/v1";
-  
-  // export const COINMARKETCAP_ENDPOINT =
-  // "https://sandbox-api.coinmarketcap.com/v1";
+// check if ENV is production or development
+export const ENV = process.env.NODE_ENV || "development";
+let URL = "https://sandbox-api.coinmarketcap.com/v1";
+let NEXT_TWEET_TIME = 1000 * 60 * 120; // 2 hours
+let NEXT_TWEET_AFTER = 1000 * 60 * 1; // 1 minutes
+if (ENV === "development") {
+  NEXT_TWEET_TIME = 1000 * 60 * 5; // 5 minutes
+  console.log("Running in development mode");
+} else {
+  console.log("Running in production mode");
+  URL = "https://pro-api.coinmarketcap.com/v1";
+}
+// export NEXT_TWEET_TIME;
+export const TWEET_INTERVAl = { NEXT_TWEET_TIME, NEXT_TWEET_AFTER };
+export const COINMARKETCAP_ENDPOINT = URL;
+
+// export const COINMARKETCAP_ENDPOINT =
+// "https://sandbox-api.coinmarketcap.com/v1";
 export const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 // supported coins
@@ -19,6 +32,6 @@ export const SUPPORTED_COINS = ["BTC", "ETH", "ATOM", "OSMO", "XPRT", "INJ"];
 
 // supported hashtags
 export const HASH_TAGS = {
-  BTC : ["#BTC", "#Bitcoin", "#Crypto", "#Cryptocurrency", "#Blockchain"],
-  ETH : ["#ETH", "#Ethereum"],
-}
+  BTC: ["#BTC", "#Bitcoin", "#Crypto", "#Cryptocurrency", "#Blockchain"],
+  ETH: ["#ETH", "#Ethereum"],
+};
--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 import { Twitter } from "./app.js";
+import { TWEET_INTERVAl } from "./config.js";
 import { generateAndSaveImage } from "./helper/test.js";
 
 import fs from "fs";
 import path from "path";
 
 function getCurrentDir() {
-    const currentUrl = import.meta.url;
-    return path.dirname(new URL(currentUrl).pathname);
+  const currentUrl = import.meta.url;
+  return path.dirname(new URL(currentUrl).pathname);
 }
 
 // Function to write log messages to a file
 export function log(message) {
-    const logFilePath = path.join(getCurrentDir(), "logs.log");
-    const timestamp = new Date().toISOString();
-    const logMessage = `[${timestamp}] ${message}\n`;
+  const logFilePath = path.join(getCurrentDir(), "logs.log");
+  const timestamp = new Date().toISOString();
+  const logMessage = `[${timestamp}] ${message}\n`;
 
-    // Append log message to the log file
-    fs.appendFile(logFilePath, logMessage, (err) => {
-        if (err) {
-            console.error("Error writing to log file:", err);
-        }
-    });
+  // Append log message to the log file
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
 }
 
-
 async function main() {
-    const twitter = new Twitter();
-    // generate image and text
-    const mesages = await generateAndSaveImage();
-    // log("Generated messages: " + JSON.stringify(mesages));
-    // console.log("MESSAGES:", mesages)
-    for (let i = 0; i < mesages.length; i++) {
-      const message = mesages[i];
+  const twitter = new Twitter();
+  // generate image and text
+  const mesages = await generateAndSaveImage();
+  // log("Generated messages: " + JSON.stringify(mesages));
+  // console.log("MESSAGES:", mesages)
+  for (let i = 0; i < mesages.length; i++) {
+    const message = mesages[i];
+    setInterval(async () => {
       await twitter.sendTweetWithMedias(message.tweetText, [message.image]);
       log("Tweet sent: " + message.tweetText);
-    }
+    }, TWEET_INTERVAl.NEXT_TWEET_AFTER);
+  }
 }
 
 main();
-setInterval(main, 1000 * 60 * 30);
-
+setInterval(main, TWEET_INTERVAl.NEXT_TWEET_TIME);
